a7177b95d99b96a3e1ef05d5a93fb71c2baba5d6
ehsanghz <56517263+ehsanghz@users.noreply.github.com>
2024-11-01 00:37:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def download_excel():
         return send_file("survey_results.xlsx", as_attachment=True, download_name="survey_results.xlsx")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)  # یا پورت دیگری که در سرور قابل دسترسی است
